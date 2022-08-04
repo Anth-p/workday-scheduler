68632a926748de0762c642c9fd07c714b6cca6f7
Anth-p <anthonypalermo5@hotmail.com>
2022-08-04 16:53:55 -0400

    local storage

--- Assets/script.js
+++ Assets/script.js
@@ -11,3 +11,15 @@ var todos = ["", "", "", "", "", "", "", "", ""];
 // Current Date
 date.append(timeDateToday);
 
+// Variables
+var container = $(".container");
+var Body = $("body");
+var date = $("#currentDay");
+var timeDateToday = moment().format('dddd, MMMM Do YYYY').toString();
+var time = moment().format('h:mm:ss a');
+
+var workHours = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"];
+var todos = ["", "", "", "", "", "", "", "", ""];
+
+// Current Date
+date.append(timeDateToday);
